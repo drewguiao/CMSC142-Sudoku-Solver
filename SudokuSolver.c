9d426eb12d09af364d4aac0e5f6a484446fbddc1
drewguiao <jbguiao@up.edu.ph>
2018-04-23 19:57:30 +0800

    Fixed printing of Grid/Signed:Drew

--- SudokuSolver.c
+++ SudokuSolver.c
@@ -1,36 +1,56 @@
 #include<stdio.h>
 #include<stdlib.h>
+#define TEMP_SIZE 5
 
 int** initializeGrid(int gridSize);
 void fillGridWithDummyData(int** sudokuGrid, int gridSize);
 void printGrid(int** sudokuGrid,int gridSize);
 
 int main(){
-    int** sudokuGrid = initializeGrid(5);
-    fillGridWithDummyData(sudokuGrid,5);
-	printGrid(sudokuGrid,5);
+
+    int** sudokuGrid = initializeGrid(TEMP_SIZE);
+    fillGridWithDummyData(sudokuGrid,TEMP_SIZE);
+	printGrid(sudokuGrid,TEMP_SIZE);
+
 }
 int** initializeGrid(int gridSize){
-    int **grid = malloc(sizeof *grid * gridSize);
-    for(int i = 0; i < gridSize; i++){
-        grid[i] = malloc(sizeof *grid[i] * gridSize);
+    int absoluteSize = gridSize * gridSize;
+    int **grid = malloc(sizeof *grid * absoluteSize);
+    for(int i = 0; i < absoluteSize; i++){
+        grid[i] = malloc(sizeof *grid[i] * absoluteSize);
     }
     return grid;
 }
 
 void fillGridWithDummyData(int** sudokuGrid, int gridSize){
-    for(int i = 0; i < gridSize; i++){
-        for(int j = 0; j < gridSize;j++){
+    int absoluteSize = gridSize * gridSize;
+    for(int i = 0; i < absoluteSize; i++){
+        for(int j = 0; j < absoluteSize;j++){
             sudokuGrid[i][j] = 1;
         }
     }
 }
 
 void printGrid(int** sudokuGrid, int gridSize){
-    for(int i = 0; i < gridSize; i++){
-        for(int j = 0; j < gridSize; j++){
+    int absoluteSize = gridSize * gridSize;
+    int separatorCounter = 0;
+    for(int i = 0; i < absoluteSize; i++){
+        for(int j = 0; j < absoluteSize; j++){
             printf("%d ",sudokuGrid[i][j]);
+            if( (j+1) % gridSize == 0 && j+1 != absoluteSize) {
+                printf("| ");
+                separatorCounter++;
+            }
         }
+
         printf("\n");
+        if((i+1) % gridSize == 0 && (i+1) != absoluteSize){
+            for(int h = 0; h < absoluteSize+separatorCounter; h++){
+                printf("- ");
+            }
+            printf("\n");
+        }
+        separatorCounter = 0;
+
     }
 }
