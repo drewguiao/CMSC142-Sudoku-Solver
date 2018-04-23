88cd38b1ce67f48872ad0bac7c227595ccaf2198
drewguiao <jbguiao@up.edu.ph>
2018-04-23 20:22:50 +0800

    Added file reading/Signed:Drew

--- SudokuSolver.c
+++ SudokuSolver.c
@@ -1,20 +1,47 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<malloc.h>
 #define TEMP_SIZE 5
 
-int** initializeGrid(int gridSize);
-void fillGridWithDummyData(int** sudokuGrid, int gridSize);
-void printGrid(int** sudokuGrid,int gridSize);
+int** initializeGrid(int subGridSize);
+void fillGridWithDummyData(int** sudokuGrid, int subGridSize);
+void printGrid(int** sudokuGrid,int subGridSize);
+
+//custom data type to store all sudokuPuzzles in txt file
+typedef struct{
+    int **sudokuGrid;
+    int subGridSize;
+}ArrayList;
 
 int main(){
 
-    int** sudokuGrid = initializeGrid(TEMP_SIZE);
-    fillGridWithDummyData(sudokuGrid,TEMP_SIZE);
-	printGrid(sudokuGrid,TEMP_SIZE);
+    int numOfPuzzles;
+    FILE *fp = fopen("input.txt","r"); //read input file
+    fscanf(fp,"%d",&numOfPuzzles); //read first number in txt file as number of SudokuPuzzles
+
+    ArrayList* puzzles = malloc(sizeof(puzzles)* numOfPuzzles); //allocate memory for puzzles
+
+    //retrieves all puzzles in the text file
+    for(int i = 0; i < numOfPuzzles; i++){
+        fscanf(fp,"%d",&puzzles[i].subGridSize); //get input from txt file as the size of the puzzle
+        puzzles[i].sudokuGrid = initializeGrid(puzzles[i].subGridSize);
+        int absoluteSize = puzzles[i].subGridSize * puzzles[i].subGridSize;
+        for(int j = 0; j < absoluteSize;j++){
+            for(int k = 0; k < absoluteSize; k++){
+                fscanf(fp,"%d",&puzzles[i].sudokuGrid[j][k]);
+            }
+        }
+        printf("PUZZLE #%d: Size: %d\n",i+1,puzzles[i].subGridSize);
+        printGrid(puzzles[i].sudokuGrid,puzzles[i].subGridSize);
+    }
+
+    //int** sudokuGrid = initializeGrid(TEMP_SIZE);
+    //fillGridWithDummyData(sudokuGrid,TEMP_SIZE);
+	//printGrid(sudokuGrid,TEMP_SIZE);
 
 }
-int** initializeGrid(int gridSize){
-    int absoluteSize = gridSize * gridSize;
+int** initializeGrid(int subGridSize){
+    int absoluteSize = subGridSize * subGridSize;
     int **grid = malloc(sizeof *grid * absoluteSize);
     for(int i = 0; i < absoluteSize; i++){
         grid[i] = malloc(sizeof *grid[i] * absoluteSize);
@@ -22,8 +49,8 @@ int** initializeGrid(int gridSize){
     return grid;
 }
 
-void fillGridWithDummyData(int** sudokuGrid, int gridSize){
-    int absoluteSize = gridSize * gridSize;
+void fillGridWithDummyData(int** sudokuGrid, int subGridSize){
+    int absoluteSize = subGridSize * subGridSize;
     for(int i = 0; i < absoluteSize; i++){
         for(int j = 0; j < absoluteSize;j++){
             sudokuGrid[i][j] = 1;
@@ -31,20 +58,20 @@ void fillGridWithDummyData(int** sudokuGrid, int gridSize){
     }
 }
 
-void printGrid(int** sudokuGrid, int gridSize){
-    int absoluteSize = gridSize * gridSize;
+void printGrid(int** sudokuGrid, int subGridSize){
+    int absoluteSize = subGridSize * subGridSize;
     int separatorCounter = 0;
     for(int i = 0; i < absoluteSize; i++){
         for(int j = 0; j < absoluteSize; j++){
             printf("%d ",sudokuGrid[i][j]);
-            if( (j+1) % gridSize == 0 && j+1 != absoluteSize) {
+            if( (j+1) % subGridSize == 0 && j+1 != absoluteSize) {
                 printf("| ");
                 separatorCounter++;
             }
         }
 
         printf("\n");
-        if((i+1) % gridSize == 0 && (i+1) != absoluteSize){
+        if((i+1) % subGridSize == 0 && (i+1) != absoluteSize){
             for(int h = 0; h < absoluteSize+separatorCounter; h++){
                 printf("- ");
             }
