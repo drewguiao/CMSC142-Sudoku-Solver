01daca56b6c62a2cc4756cb3c84c51daf35920e9
drewguiao <jbguiao@up.edu.ph>
2018-05-21 17:55:53 +0800

    Added number of solutions and shows current mode solution/Signed:Drew

--- Java/SolutionsFrame.java
+++ Java/SolutionsFrame.java
@@ -2,18 +2,22 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
 import java.awt.GridLayout;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-
+import java.util.List;
 class SolutionsFrame extends JFrame implements ActionListener{
   private static final int JFRAME_SIZE = 500;
 
   private JPanel boardPanel	  = new JPanel();
   private JPanel buttonsPanel	= new JPanel();
+  
+  private JPanel numberOfSolutionsPanel = new JPanel();
+  private JLabel numberOfSolutionsTextField = new JLabel();
 
   private JButton prev 	= new JButton("Previous");
 	private JButton next 	= new JButton("Next");
@@ -21,27 +25,36 @@ class SolutionsFrame extends JFrame implements ActionListener{
 
   private JLabel solLabel = new JLabel();
 
-  private Puzzle puzzle;
   private int boardSize;
   private int solIndex;
 
-  public SolutionsFrame(Puzzle puzzle){
-    super("Puzzle #" + puzzle.getPuzzleNumber() + " Solutions");
-    this.puzzle = puzzle;
-    this.boardSize = puzzle.getBoardSize();
+
+  
+  private List<int[][] > solutions;
+
+ 
+
+  public SolutionsFrame(List<int[][]> solutions, int subGridSize){
+    super("Solutions:");
+    this.solutions = solutions;
+    this.boardSize = subGridSize * subGridSize;
     this.board = new JButton[this.boardSize][this.boardSize];
     this.solIndex = 0;
 
     this.setSize(JFRAME_SIZE, JFRAME_SIZE);
-		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		this.setLayout(new BorderLayout());
-		this.setComponents();
-		this.pack();
+    this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    this.setLayout(new BorderLayout());
+    this.setComponents();
+    this.pack();
     this.setLocationRelativeTo(null);
-		this.setVisible(true);
+    this.setVisible(true);
   }
 
   private void setComponents(){
+    this.numberOfSolutionsTextField.setEnabled(false);
+    this.numberOfSolutionsTextField.setText("Number of solutions: "+this.solutions.size());
+    this.numberOfSolutionsPanel.add(this.numberOfSolutionsTextField);
+
     this.boardPanel.setLayout(new GridLayout(this.boardSize,this.boardSize));
 		this.setBoard();
     
@@ -53,6 +66,7 @@ class SolutionsFrame extends JFrame implements ActionListener{
     this.buttonsPanel.add(this.prev);
     this.buttonsPanel.add(this.next);
 
+    this.add(numberOfSolutionsPanel, BorderLayout.PAGE_START);
     this.add(boardPanel, BorderLayout.CENTER);
 		this.add(buttonsPanel, BorderLayout.PAGE_END);
   }
@@ -60,7 +74,7 @@ class SolutionsFrame extends JFrame implements ActionListener{
   private void setBoard(){
     for(int i=0; i<this.boardSize; i++){
       for(int j=0; j<this.boardSize; j++){
-        this.board[i][j] = new JButton(Integer.toString(this.puzzle.getSolutions().get(this.solIndex)[i][j]));
+        this.board[i][j] = new JButton(Integer.toString(this.solutions.get(this.solIndex)[i][j]));
         this.board[i][j].setFocusPainted(false);
         this.board[i][j].setPreferredSize(new Dimension(400/this.boardSize, 300/this.boardSize));
     		this.board[i][j].setBackground(Color.WHITE);
@@ -72,7 +86,7 @@ class SolutionsFrame extends JFrame implements ActionListener{
   private void update(){
     for(int i=0; i<this.boardSize; i++)
       for(int j=0; j<this.boardSize; j++)
-        this.board[i][j].setText(Integer.toString(this.puzzle.getSolutions().get(this.solIndex)[i][j]));
+        this.board[i][j].setText(Integer.toString(this.solutions.get(this.solIndex)[i][j]));
     this.solLabel.setText("Solution #" + (solIndex+1));
   }
 
@@ -81,7 +95,7 @@ class SolutionsFrame extends JFrame implements ActionListener{
   	JButton button = ((JButton)(e.getSource()));
 
   	if(button==this.prev && this.solIndex-1>=0) this.solIndex--;
-  	else if(button==this.next && this.solIndex+1<this.puzzle.getNumberOfSolutions()) this.solIndex++;
+  	else if(button==this.next && this.solIndex+1<this.solutions.size()) this.solIndex++;
   	this.update();
   }
 
